3248ab18c6919c5bf796517548c37604d4dc4bd5
Sergio-hdD <sergio-hdd@hotmail.com>
2022-08-11 02:22:40 -0300

    Práctica 1. C

--- app/src/main/java/com/sergio/examenes/MainActivityPractica.java
+++ app/src/main/java/com/sergio/examenes/MainActivityPractica.java
@@ -2,6 +2,7 @@ package com.sergio.examenes;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,15 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivityPractica extends AppCompatActivity {
 
     public void buttonCalculatePress(View v){
-        Toast.makeText(this, "Se presionó el botón", Toast.LENGTH_SHORT).show();
+        EditText et1, et2;
+        Integer num1, num2, prom;
+        et1 = (EditText) findViewById(R.id.editNota1);
+        et2 = (EditText) findViewById(R.id.editNota2);
+        num1 = Integer.parseInt(et1.getText().toString());
+        num2 = Integer.parseInt(et2.getText().toString());
+        prom = (num1 + num2) / 2;
+
+        Toast.makeText(this, "El promedio es: "+prom, Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
