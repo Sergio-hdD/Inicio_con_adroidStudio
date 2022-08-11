446763ebf52d1b662a3ec62d42c7fafb43a345d6
Sergio-hdD <sergio-hdd@hotmail.com>
2022-08-11 02:14:32 -0300

    Práctica 1. B

--- app/src/main/java/com/sergio/examenes/MainActivityPractica.java
+++ app/src/main/java/com/sergio/examenes/MainActivityPractica.java
@@ -1,11 +1,16 @@
 package com.sergio.examenes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivityPractica extends AppCompatActivity {
 
+    public void buttonCalculatePress(View v){
+        Toast.makeText(this, "Se presionó el botón", Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
