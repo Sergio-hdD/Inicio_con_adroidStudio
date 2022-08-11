1c739945adad8378902e162689278326c254b88b
Sergio-hdD <sergio-hdd@hotmail.com>
2022-08-11 02:03:31 -0300

    Alistando archivos separados para prácticas

---
+++ app/src/main/java/com/sergio/examenes/MainActivityPractica.java
@@ -0,0 +1,14 @@
+package com.sergio.examenes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityPractica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practica);
+    }
+}
