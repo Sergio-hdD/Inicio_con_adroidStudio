47c818cc2daef2c10b1ae99e90ae5990e7d57478
Sergio-hdD <sergio-hdd@hotmail.com>
2022-08-17 22:39:43 -0300

    Ordenando la clase de acuerdo a lo pedido

--- app/src/main/java/com/sergio/examenes/MainActivityPractica.java
+++ app/src/main/java/com/sergio/examenes/MainActivityPractica.java
@@ -2,6 +2,7 @@ package com.sergio.examenes;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -9,20 +10,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivityPractica extends AppCompatActivity {
 
-    public void buttonCalculatePress(View v){
-        EditText et1, et2;
-        Integer num1, num2, prom;
-        et1 = (EditText) findViewById(R.id.editNota1);
-        et2 = (EditText) findViewById(R.id.editNota2);
-        num1 = Integer.parseInt(et1.getText().toString());
-        num2 = Integer.parseInt(et2.getText().toString());
-        prom = (num1 + num2) / 2;
+    private Button btnCalcularPromedio;
 
-        Toast.makeText(this, "El promedio es: "+prom, Toast.LENGTH_SHORT).show();
-    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practica);
+
+        btnCalcularPromedio = findViewById(R.id.btnPromedio);
+
+        btnCalcularPromedio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNote = (EditText) findViewById(R.id.editNota1);
+                EditText secondNote = (EditText) findViewById(R.id.editNota2);
+
+                int average = (Integer.parseInt(firstNote.getText().toString()) + Integer.parseInt(secondNote.getText().toString())) / 2;
+
+                Toast.makeText(MainActivityPractica.this, "El promedio es: " + String.valueOf(average), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+    
 }
