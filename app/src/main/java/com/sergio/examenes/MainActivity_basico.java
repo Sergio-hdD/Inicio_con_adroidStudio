5978917db45819d7c4f91361777fe1b965dc346a
Sergio-hdD <sergio-hdd@hotmail.com>
2022-08-20 18:42:23 -0300

    Agrego un MainActivity básico para probar diferentes activitys

---
+++ app/src/main/java/com/sergio/examenes/MainActivity_basico.java
@@ -0,0 +1,19 @@
+package com.sergio.examenes;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity_basico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
