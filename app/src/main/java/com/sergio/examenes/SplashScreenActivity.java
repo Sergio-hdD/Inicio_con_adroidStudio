2a1831c4dcb5c633cc58a5fe727b4a50859f0f04
Sergio-hdD <sergio-hdd@hotmail.com>
2022-08-23 12:43:15 -0300

    Práctica 3. A

---
+++ app/src/main/java/com/sergio/examenes/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.sergio.examenes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final long DELAYED_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); //Para cerrar (matar) la vista que dejo
+            }
+        }, DELAYED_TIME);
+    }
+}
